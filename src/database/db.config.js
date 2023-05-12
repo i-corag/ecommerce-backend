b4f8880e8ce9473ea11be1dd106d99bf7ab9810e
i-corag <icoraglio@gmail.com>
2023-05-12 15:54:52 +0200

    first commit

--- src/database/db.config.js
+++ src/database/db.config.js
@@ -7,7 +7,7 @@ const db = new Sequelize(
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
-    host: 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     port: process.env.DB_PORT,
   }
